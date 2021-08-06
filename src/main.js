edf185e480529c64ea073dda0dfd8e36b82e8f94
DonDejvo <dada542cz@gmail.com>
2021-08-06 13:59:24 +0200

    loader intro updated

--- src/main.js
+++ src/main.js
@@ -15,19 +15,19 @@ class Game {
                 document.getElementById("loadingProgress").textContent = percents + "%";
                 console.log(`${percents}% ... ${obj.path}`);
             })
-            .SetPath("https://raw.githubusercontent.com/DonDejvo/GrottoEscape/main")
-            .AddImage("tileset-image", "res/assets/png/environment-tiles.png")
-            .AddImage("items", "res/assets/png/items.png")
-            .AddImage("hp-bar", "res/assets/png/meter.png")
-            .AddImage("bat", "res/assets/spritesheets/bat.png")
-            .AddImage("ghost", "res/assets/spritesheets/ghost.png")
-            .AddImage("player", "res/assets/spritesheets/player.png")
-            .AddImage("skeleton", "res/assets/spritesheets/skeleton.png")
-            .AddImage("enemies", "res/assets/spritesheets/enemies.png")
-            .AddAudio("jump-sound", "res/audio/sounds/jump.wav")
-            .AddAudio("laser-sound", "res/audio/sounds/laser.wav")
-            .AddAudio("pickup-sound", "res/audio/sounds/pickup.wav")
-            .AddJSON("tileset", "res/layouts/grottoEnvironment.json")
+            .SetPath("https://raw.githubusercontent.com/DonDejvo/GrottoEscape/main/res")
+            .AddImage("tileset-image", "assets/png/environment-tiles.png")
+            .AddImage("items", "assets/png/items.png")
+            .AddImage("hp-bar", "assets/png/meter.png")
+            .AddImage("bat", "assets/spritesheets/bat.png")
+            .AddImage("ghost", "assets/spritesheets/ghost.png")
+            .AddImage("player", "assets/spritesheets/player.png")
+            .AddImage("skeleton", "assets/spritesheets/skeleton.png")
+            .AddImage("enemies", "assets/spritesheets/enemies.png")
+            .AddAudio("jump-sound", "audio/sounds/jump.wav")
+            .AddAudio("laser-sound", "audio/sounds/laser.wav")
+            .AddAudio("pickup-sound", "audio/sounds/pickup.wav")
+            .AddJSON("tileset", "layouts/grottoEnvironment.json")
             .Load((data) => {
 
             });
