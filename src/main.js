7ba0e18365cf1f65b6019f57be55b72ac2a0d23a
DonDejvo <dada542cz@gmail.com>
2021-08-06 13:58:12 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -11,26 +11,23 @@ class Game {
         loader
             .OnProgress((progress, obj) => {
                 const percents = parseInt(progress * 100);
-                document.querySelector(".progressBar_progress").getElementsByClassName.width = percents + "%";
+                document.querySelector(".progressBar_progress").style.width = percents + "%";
                 document.getElementById("loadingProgress").textContent = percents + "%";
                 console.log(`${percents}% ... ${obj.path}`);
             })
-            .SetPath("res/assets/png")
-            .AddImage("tileset-image", "environment-tiles.png")
-            .AddImage("items", "items.png")
-            .AddImage("hp-bar", "meter.png")
-            .SetPath("res/assets/spritesheets")
-            .AddImage("bat", "bat.png")
-            .AddImage("ghost", "ghost.png")
-            .AddImage("player", "player.png")
-            .AddImage("skeleton", "skeleton.png")
-            .AddImage("enemies", "enemies.png")
-            .SetPath("res/audio/sounds")
-            .AddAudio("jump-sound", "jump.wav")
-            .AddAudio("laser-sound", "laser.wav")
-            .AddAudio("pickup-sound", "pickup.wav")
-            .SetPath("res/layouts")
-            .AddJSON("tileset", "grottoEnvironment.json")
+            .SetPath("https://raw.githubusercontent.com/DonDejvo/GrottoEscape/main")
+            .AddImage("tileset-image", "res/assets/png/environment-tiles.png")
+            .AddImage("items", "res/assets/png/items.png")
+            .AddImage("hp-bar", "res/assets/png/meter.png")
+            .AddImage("bat", "res/assets/spritesheets/bat.png")
+            .AddImage("ghost", "res/assets/spritesheets/ghost.png")
+            .AddImage("player", "res/assets/spritesheets/player.png")
+            .AddImage("skeleton", "res/assets/spritesheets/skeleton.png")
+            .AddImage("enemies", "res/assets/spritesheets/enemies.png")
+            .AddAudio("jump-sound", "res/audio/sounds/jump.wav")
+            .AddAudio("laser-sound", "res/audio/sounds/laser.wav")
+            .AddAudio("pickup-sound", "res/audio/sounds/pickup.wav")
+            .AddJSON("tileset", "res/layouts/grottoEnvironment.json")
             .Load((data) => {
 
             });
